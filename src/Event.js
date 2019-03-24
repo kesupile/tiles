a21d3e439b073b24ffd40a3b5054c31ec0e85e7c
Kesupile <kesupilek@googlemail.com>
2019-03-24 11:39:43 +0000

    - method names

--- src/Event.js
+++ src/Event.js
@@ -5,47 +5,17 @@ export default class Event {
     this._srcFn = srcFn;
     this.nextQueue = [];
     this.delayedFrameCallbacks = {};
-    this.flipNowQueue = [];
 
     /** for debugging purposes */
     console.time("ANIMATION_TIME");
     global.Tiles._executionTimes = [];
   }
 
-  addToQueue = (fn, coords) => {
-    this.flipped[coords] = true;
-    this.nextQueue.push(fn);
-  };
-
-  addToFlipNow = (fn, coords) => {
-    this.flipNowQueue.push(fn);
-    this.flipped[coords] = true;
-  };
-
-  includes = coords =>
-    !!this.flipped[typeof coords === "object" ? coords.coords : coords];
-
-  startFrames = n => {
-    if (n === 1) this._startTime = new Date().getTime();
-    this.frame = n;
-    setTimeout(this.onNextFrame, 17, n);
-  };
-
-  endFrames = () => {
+  _endFrames = () => {
     this.active = false;
   };
 
-  delay = (frameDelay, fn) => {
-    /** @@NOTE: might we not want to add the element to the queue as soon as the delayed callback has been executed?? */
-    const targetFrame = this.frame + frameDelay;
-    if (this.delayedFrameCallbacks.hasOwnProperty(targetFrame)) {
-      this.delayedFrameCallbacks[targetFrame].push(fn);
-    } else {
-      this.delayedFrameCallbacks[targetFrame] = [fn];
-    }
-  };
-
-  onNextFrame = n => {
+  _onNextFrame = n => {
     // if no more fns in the queue or no delayed fns stop animation
     if (
       !this.nextQueue.length &&
@@ -75,5 +45,29 @@ export default class Event {
     delete this.delayedFrameCallbacks[n];
 
     this.startFrames(n + 1);
+  };
+
+  addToQueue = (fn, coords) => {
+    this.flipped[coords] = true;
+    this.nextQueue.push(fn);
+  };
+
+  startFrames = n => {
+    if (n === 1) this._startTime = new Date().getTime();
+    this.frame = n;
+    setTimeout(this._onNextFrame, 17, n);
+  };
+
+  includes = coords =>
+    !!this.flipped[typeof coords === "object" ? coords.coords : coords];
+
+  delay = (frameDelay, fn) => {
+    /** @@NOTE: might we not want to add the element to the queue as soon as the delayed callback has been executed?? */
+    const targetFrame = this.frame + frameDelay;
+    if (this.delayedFrameCallbacks.hasOwnProperty(targetFrame)) {
+      this.delayedFrameCallbacks[targetFrame].push(fn);
+    } else {
+      this.delayedFrameCallbacks[targetFrame] = [fn];
+    }
   };
 }
